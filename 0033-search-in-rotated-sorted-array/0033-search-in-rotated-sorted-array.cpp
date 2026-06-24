959090bb03eaeed970ec282517a6b3fe06adddc7
Siddharth Surana <sddhrthsrn119@gmail.com>
2026-06-24 16:14:53 +0530

    Time: 0 ms (100%), Space: 15.1 MB (67.13%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,66 @@
+class Solution {
+    int Pivot_Index(vector<int>& nums){
+        int start = 0;
+        int end = nums.size()-1;
+        int mid=start+(end-start)/2;
+        
+        if(nums[start]<nums[end]){
+            return start;
+        }
+        
+        while(start<end){
+            if(nums[mid]>=nums[0]){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }
+            mid=start+(end-start)/2;
+        }
+        return mid;
+    }
+
+public:
+    int search(vector<int>& nums, int target) {
+
+        int PivotElement=Pivot_Index(nums);
+
+        if(nums[PivotElement]<=target && target<=nums[nums.size()-1]){
+            int start=PivotElement;
+            int end=nums.size()-1;
+            int mid=start+(end-start)/2;
+
+            while(start<=end){
+                if(nums[mid]>target){
+                    end=mid-1;
+                }
+                else if(nums[mid]<target){
+                    start=mid+1;
+                }
+                else if(nums[mid]==target){
+                    return mid;
+                }
+                mid=start+(end-start)/2;
+            }
+        }
+        else{
+            int start=0;
+            int end=PivotElement-1;
+            int mid=start+(end-start)/2;
+
+            while(start<=end){
+                if(nums[mid]>target){
+                    end=mid-1;
+                }
+                else if(nums[mid]<target){
+                    start=mid+1;
+                }
+                else if(nums[mid]==target){
+                    return mid;
+                }
+                mid=start+(end-start)/2;
+            }
+        }
+        return -1;
+    }
+};
